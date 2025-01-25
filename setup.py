334596058ca07dfb71c53e1df2cc122f6f5f8358
miro <jarbasai@mailfence.com>
2025-01-25 04:05:57 +0000

    fix:packaging
    files not being included properly

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ PLUGIN_ENTRY_POINT = f'{SKILL_NAME.lower()}.{SKILL_AUTHOR.lower()}={SKILL_PKG}:{
 
 
 def find_resource_files():
-    resource_base_dirs = ("locale")
+    resource_base_dirs = ("locale",)
     base_dir = path.dirname(__file__)
     package_data = ["*.json"]
     for res in resource_base_dirs:
